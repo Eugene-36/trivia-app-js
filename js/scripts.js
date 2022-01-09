e9454fbaac25c00c21868915c7cf02db19ffb89b
Eugene-36 <67147219+Eugene-36@users.noreply.github.com>
2022-01-09 21:38:41 +0200

    complete

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,35 @@
 const game = document.getElementById('game'),
   scoreDisplay = document.getElementById('score');
 
-const film = 11;
+let score = 0;
+
+const genres = [
+  {
+    name: 'Books',
+    id: 10,
+  },
+  {
+    name: 'Film',
+    id: 11,
+  },
+  {
+    name: 'Music',
+    id: 12,
+  },
+  {
+    name: 'Video Games',
+    id: 15,
+  },
+];
 const levels = ['easy', 'medium', 'hard'];
 
-function addGenre() {
+function addGenre(genre) {
+  const { name, id } = genre;
+  console.log('genres', id);
+
   const column = document.createElement('div');
   column.classList.add('genre-column');
-  column.innerHTML = `this is a genre`;
+  column.innerHTML = name;
   game.append(column);
 
   levels.forEach((level) => {
@@ -15,12 +37,91 @@ function addGenre() {
     card.classList.add('card');
     column.append(card);
 
+    if (level === 'easy') {
+      card.innerHTML = 100;
+    }
+
+    if (level === 'medium') {
+      card.innerHTML = 200;
+    }
+
+    if (level === 'hard') {
+      card.innerHTML = 300;
+    }
+
     fetch(
-      `https://opentdb.com/api.php?amount=1&category=11&difficulty=${level}&type=boolean`
+      `https://opentdb.com/api.php?amount=1&category=${id}&difficulty=${level}&type=boolean`
     )
       .then((respons) => respons.json())
-      .then((data) => console.log(data));
+      .then((data) => {
+        console.log('data', data);
+        card.setAttribute('data-question', data.results[0].question);
+        card.setAttribute('data-answer', data.results[0].correct_answer);
+        card.setAttribute('data-value', card.getInnerHTML());
+      })
+      .then((done) => card.addEventListener('click', flipCard));
   });
 }
 
-addGenre();
+//addGenre(genres[0]);
+genres.forEach((genre) => addGenre(genre));
+
+function flipCard() {
+  this.innerHTML = '';
+  this.style.fontSize = '15px';
+  document.createElement('div');
+  const textDisplay = document.createElement('div');
+  const trueButton = document.createElement('button');
+  const falseButton = document.createElement('button');
+
+  trueButton.innerHTML = 'True';
+  falseButton.innerHTML = 'False';
+
+  // style for the button
+  trueButton.classList.add('true-button');
+  falseButton.classList.add('false-button');
+  //===========
+  trueButton.addEventListener('click', getResult);
+  falseButton.addEventListener('click', getResult);
+  //===========
+
+  textDisplay.innerHTML = this.getAttribute('data-question');
+  this.append(textDisplay, trueButton, falseButton);
+  console.log('cheacking for the click ');
+
+  const allCards = Array.from(document.querySelectorAll('.card'));
+
+  allCards.forEach((card) => card.removeEventListener('click', flipCard));
+}
+
+function getResult() {
+  const allCards = Array.from(document.querySelectorAll('.card'));
+  allCards.forEach((card) => card.addEventListener('click', flipCard));
+
+  const cardOfButton = this.parentElement;
+
+  if (cardOfButton.getAttribute('data-answer') === this.innerHTML) {
+    console.log('its a match');
+    score = score + parseInt(cardOfButton.getAttribute('data-value'));
+    scoreDisplay.innerHTML = score;
+    cardOfButton.classList.add('correct-answer');
+
+    setTimeout(() => {
+      while (cardOfButton.firstChild) {
+        cardOfButton.removeChild(cardOfButton.lastChild);
+      }
+      cardOfButton.innerHTML = cardOfButton.getAttribute('data-value');
+    }, 100);
+  } else {
+    cardOfButton.classList.add('wrong-answer');
+
+    setTimeout(() => {
+      while (cardOfButton.firstChild) {
+        cardOfButton.removeChild(cardOfButton.lastChild);
+      }
+      cardOfButton.innerHTML = 0;
+    }, 100);
+  }
+
+  cardOfButton.removeEventListener('click', flipCard);
+}
